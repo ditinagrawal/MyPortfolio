6f218d6d0de6a9814dbfa1ce9d82cc274217a842
Ditin Agrawal <ditin.agrawal05@gmail.com>
2023-08-31 21:06:12 +0530

    Some Code Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import './App.css'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import Home from './pages/Home/Home'
-import About from './pages/About/About'
-import Projects from './pages/Projects/Projects'
-import Skills from './pages/Skills/Skills'
-import Contact from './pages/Contact/Contact'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Projects from "./pages/Projects/Projects";
+import Skills from "./pages/Skills/Skills";
+import Contact from "./pages/Contact/Contact";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Header/>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/projects' element={<Projects/>}/>
-          <Route path='/skills' element={<Skills/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-        </Routes>
-      <Footer/>
-    </BrowserRouter>
-  )
-}
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/skills" element={<Skills />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
+};
 
-export default App
+export default App;
